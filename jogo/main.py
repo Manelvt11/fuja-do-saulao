4aa0505628cf099e5ec8be44df21c23953d41c2c
Manoel Vitor <brito.nascimento@escolar.ifrn.edu.br>
2026-03-29 20:48:27 -0300

    Começando sistema inicial de colisão e ajustes no player

--- jogo/main.py
+++ jogo/main.py
@@ -1,16 +1,16 @@
 import pygame
-from player import Player 
+from player import Player
 
-pygame.init() 
+pygame.init()
 
-tela = pygame.display.set_mode((800, 600)) 
-pygame.display.set_caption("Fuja do Saulão") 
+tela = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Fuja do Saulão")
 
-fundo = pygame.image.load("assets/lab.png").convert() 
-novo_tamanho_fundo = (800, 600) 
-fundo_redimensionado = pygame.transform.scale(fundo, novo_tamanho_fundo) 
+fundo = pygame.image.load("assets/lab.png").convert()
+novo_tamanho_fundo = (800, 600)
+fundo_redimensionado = pygame.transform.scale(fundo, novo_tamanho_fundo)
 
-clock = pygame.time.Clock() 
+clock = pygame.time.Clock()
 FPS = 60
 
 LARGURA = 800
@@ -22,21 +22,34 @@ altura_player = 140
 x = LARGURA // 2 - largura_player // 2
 y = ALTURA // 2 - altura_player // 2
 
-player = Player(x, y, 5)
+player = Player(50, 50, 5)
 
-rodando = True 
-while rodando: 
-    for evento in pygame.event.get(): 
-        if evento.type == pygame.QUIT: 
-            rodando = False 
-            
-    tela.blit(fundo_redimensionado, (0, 0)) 
+mesa1 = pygame.Rect(140, 230, 205, 50)  # mesa de cima esquerda
+mesa2 = pygame.Rect(455, 230, 200, 50)  # mesa de cima direita
 
-    player.mover()
+mesa3 = pygame.Rect(140, 420, 205, 33)  # baixo esquerda
+mesa4 = pygame.Rect(455, 420, 200, 33)  # baixo direita
+
+obstaculos = [mesa1, mesa2, mesa3, mesa4]
+
+rodando = True
+while rodando:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            rodando = False
+
+    tela.blit(fundo_redimensionado, (0, 0))
+
+    pygame.draw.rect(tela, (255,0 ,0), mesa1)
+    pygame.draw.rect(tela, (255,0 ,0), mesa2)
+    pygame.draw.rect(tela, (255,0 ,0), mesa3)
+    pygame.draw.rect(tela, (255,0 ,0), mesa4)
+
+    player.mover(obstaculos)
     player.desenhar(tela)
 
     pygame.display.flip()
 
-    clock.tick(FPS) 
+    clock.tick(FPS)
 
 pygame.quit()
--- jogo/player.py
+++ jogo/player.py
@@ -2,30 +2,51 @@ import pygame
 
 class Player:
     def __init__(self, x, y, velocidade):
-        self.x = x
-        self.y = y
+        self.rect = pygame.Rect(x, y + 40, 80, 90)
         self.velocidade = velocidade
 
         self.imagem = pygame.image.load("assets/player.png").convert_alpha()
         self.imagem = pygame.transform.scale(self.imagem, (150, 140))
 
-    def mover(self):
+    def mover(self, obstaculos):
         teclado = pygame.key.get_pressed()
 
+        posicao_antiga_x = self.rect.x
+
         if teclado[pygame.K_LEFT]:
-            self.x -= self.velocidade
+            self.rect.x -= self.velocidade
         
         if teclado[pygame.K_RIGHT]:
-            self.x += self.velocidade
+            self.rect.x += self.velocidade
+
+        for obs in obstaculos:
+            if self.rect.colliderect(obs):
+                self.rect.x = posicao_antiga_x
+
+        posicao_antiga_y = self.rect.y
 
         if teclado[pygame.K_UP]:
-            self.y -= self.velocidade
+            self.rect.y -= self.velocidade
 
         if teclado[pygame.K_DOWN]:
-            self.y += self.velocidade
+            self.rect.y += self.velocidade
+
+        for obs in obstaculos:
+            if self.rect.colliderect(obs):
+                self.rect.y = posicao_antiga_y
+
+        if self.rect.left < 20:
+            self.rect.left = 20
+
+        if self.rect.top < 20:
+            self.rect.top = 20
+
+        if self.rect.right > 800 - 20:
+            self.rect.right = 800 - 20
+
+        if self.rect.bottom > 600 - 40:
+            self.rect.bottom = 600 - 40
+
 
     def desenhar(self, tela):
-        tela.blit(self.imagem, (self.x, self.y))
-
-        
-
+        tela.blit(self.imagem, (self.rect.x - 30, self.rect.y - 40))
