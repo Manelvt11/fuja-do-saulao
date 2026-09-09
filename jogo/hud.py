8de093e81c2311ddd19cba460a6d02bc54fe0c02
Manelvt11 <brito.nascimento@escolar.ifrn.edu.br>
2026-09-09 09:09:12 -0300

    adicionando hud do inventario

--- jogo/hud.py
+++ jogo/hud.py
@@ -26,6 +26,25 @@ class HUD:
         self.tempo_frame = 0
         self.vida_anterior = 3
 
+        self.inventario_hud = pygame.image.load(
+            os.path.join(BASE_DIR, "assets", "hud", "inventarioHud.png")
+        ).convert_alpha()
+
+        self.inventario_largura = 270
+        self.inventario_altura = 155
+
+        self.inventario_hud = pygame.transform.smoothscale(
+            self.inventario_hud, (self.inventario_largura, self.inventario_altura)
+        )
+
+        self.posicoes_slots = [
+            (66, 77),
+            (135, 77),
+            (204, 77)
+        ]
+
+        self.tamanho_icone = 65
+
     def desenhar(self, tela, jogador):
         if jogador.vida < self.vida_anterior:
             self.animando = True
@@ -59,15 +78,21 @@ class HUD:
 
             tela.blit(imagem, (10 + i * 50, 10))
 
-        tamanho_icone = 44
-        espacamento = 6
-        y_base = tela.get_height() - tamanho_icone - 10
+        x_inventario = (tela.get_width() - self.inventario_largura) // 2
+        y_inventario = (tela.get_height() - self.inventario_altura - 5)
+
+        tela.blit(self.inventario_hud, (x_inventario, y_inventario))
 
         for i, item in enumerate(jogador.inventario.itens):
-            icone = obter_icone(item.nome, tamanho=tamanho_icone)
-            x = 10 + i * (tamanho_icone + espacamento)
+            if i >= 3:
+                break
 
-            slot = pygame.Surface((tamanho_icone, tamanho_icone), pygame.SRCALPHA)
-            slot.fill((0, 0, 0, 90))
-            tela.blit(slot, (x, y_base))
-            tela.blit(icone, (x, y_base))
+            slot_x, slot_y = self.posicoes_slots[i]
+
+            icone = obter_icone(item.nome, tamanho=self.tamanho_icone)
+
+            icone_x = (x_inventario + slot_x - self.tamanho_icone // 2)
+
+            icone_y = (y_inventario + slot_y - self.tamanho_icone // 2)
+
+            tela.blit(icone, (icone_x, icone_y))
